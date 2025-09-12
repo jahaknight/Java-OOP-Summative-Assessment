61ce53bd952e363a351f559b970646bd995ba275
SlinkyGoat <adamustby@gmail.com>
2025-09-12 16:30:57 -0500

    tests

--- ShoppingCartOOPSummativeAssessment/src/org/example/cart/Cart.java
+++ ShoppingCartOOPSummativeAssessment/src/org/example/cart/Cart.java
@@ -1,12 +1,15 @@
 package org.example.cart;
 
+import org.example.model.CartLine;
 import org.example.model.Item;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 public interface Cart {
     public void addItemToCart(Item item, int quantity);
     public void removeItemFromCart(Item item, int quantity);
     public BigDecimal getSubtotal();
-    public void checkout();
+//    public List<CartLine> checkout();
+    public BigDecimal checkout();
 }
