5e9f664330b9bc629002bb220e303bd87d8514bc
SlinkyGoat <adamustby@gmail.com>
2025-09-12 14:05:26 -0500

    test commit

--- ShoppingCartOOPSummativeAssessment/src/org/example/cart/Cart.java
+++ ShoppingCartOOPSummativeAssessment/src/org/example/cart/Cart.java
@@ -3,8 +3,8 @@ package org.example.cart;
 import org.example.model.Item;
 
 public interface Cart {
-    public void addItemToCart(Item item);
-    public void removeItemFromCart(Item item);
+    public void addItemToCart(Item item, int quantity);
+    public void removeItemFromCart(Item item, int quantity);
     public double getSubtotal();
     public void checkout();
 }
