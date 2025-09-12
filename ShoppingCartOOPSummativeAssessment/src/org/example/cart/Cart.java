bf40da679165f0f7a4b126d55fbee6e035c934e2
SlinkyGoat <adamustby@gmail.com>
2025-09-12 13:07:23 -0500

    added implementations and their methods

--- ShoppingCartOOPSummativeAssessment/src/org/example/cart/Cart.java
+++ ShoppingCartOOPSummativeAssessment/src/org/example/cart/Cart.java
@@ -1,4 +1,10 @@
 package org.example.cart;
 
+import org.example.model.Item;
+
 public interface Cart {
+    public void addItemToCart(Item item);
+    public void removeItemFromCart(Item item);
+    public double getSubtotal();
+    public void checkout();
 }
--- ShoppingCartOOPSummativeAssessment/src/org/example/cart/InMemoryCart.java
+++ ShoppingCartOOPSummativeAssessment/src/org/example/cart/InMemoryCart.java
@@ -1,4 +1,25 @@
 package org.example.cart;
 
+import org.example.model.Item;
+
 public class InMemoryCart implements Cart{
+    @Override
+    public void addItemToCart(Item item) {
+
+    }
+
+    @Override
+    public void removeItemFromCart(Item item) {
+
+    }
+
+    @Override
+    public double getSubtotal() {
+        return 0;
+    }
+
+    @Override
+    public void checkout() {
+
+    }
 }
