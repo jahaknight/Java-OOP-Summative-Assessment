ddf6b0ee1927e77816722b40f66fc9293bd553d9
SlinkyGoat <adamustby@gmail.com>
2025-09-12 15:14:03 -0500

    commit

--- ShoppingCartOOPSummativeAssessment/src/org/example/view/MenuController.java
+++ ShoppingCartOOPSummativeAssessment/src/org/example/view/MenuController.java
@@ -4,11 +4,15 @@ import org.example.cart.Cart;
 import org.example.cart.InMemoryCart;
 import org.example.catalog.Catalog;
 import org.example.catalog.StaticCatalog;
+import org.example.model.CartLine;
+import org.example.model.Item;
+
+import java.util.List;
 
 public class MenuController {
 
-    private Cart myCart;
-    private Catalog catalog;
+    private InMemoryCart myCart;
+    private StaticCatalog catalog;
     private ConsoleIO console;
 
     public MenuController(){
@@ -52,37 +56,36 @@ public class MenuController {
     }
 
     private void displayCart(){
-        // TODO
-        /*
-        get all items in cart and display as such
-        ProductName
-            Quantity: [quantity]
-            Cost: $[cost]
-         */
+        List<CartLine> items = myCart.getLines();
+        for(CartLine item : items){
+            console.writeMessage(item.toString());
+        }
 
     }
 
+    /**
+     * Prints a list of all items in the catalog then prompts user for
+     * the Item and quantity. Then inputted amount is then added to the cart.
+     */
     private void addItemToCart(){
-        // TODO
-        /*
-        ask user to choose item from catalog
-        ask user quantity they would like to add
-        give user price and ask for confirmation
-        if confirm add item quantity to cart, if deny exit method
-
-        have option to return back to menu
-         */
+        List<Item> catalogItems = catalog.getCatalogItems();
+        // print available catalog items
+        printOrderedItems(catalogItems);
+        int itemIndex = console.getIntegerInputInRange("What item would you like to add to your cart:", 1, catalogItems.size());
+        itemIndex--; // adjust for 0-index
+        Item itemToAdd = catalogItems.get(itemIndex);
+        int quantity = console.getIntegerInput("How many " + itemToAdd.getName() + "s would you like to add: ");
+        myCart.addItemToCart(itemToAdd, quantity);
     }
 
     private void removeItemFromCart(){
-        // TODO
-        /*
-        ask user which item they would like to remove
-        ask quantity they would like to remove
-        ask for confirmation
-
-        have option to return back to menu
-         */
+        List<CartLine> cartLineList = myCart.getLines();
+        printOrderedCartItems(cartLineList);
+        int itemIndex = console.getIntegerInputInRange("What item would you like to remove from your cart:", 1, cartLineList.size());
+        itemIndex--;
+        Item itemToRemove = cartLineList.get(itemIndex).getItem();
+        int quantity = console.getIntegerInput("How many " + itemToRemove.getName() + "s would you like to remove: ");
+        myCart.removeItemFromCart(itemToRemove, quantity);
     }
 
     private void checkout(){
@@ -91,6 +94,22 @@ public class MenuController {
         display all items and final cost
         empty entire cart
          */
+    }
+
+    private void printOrderedItems(List<Item> items){
+        int i = 1;
+        for(Item item : items){
+            console.writeMessage(i + ". " + item.getName());
+            i++;
+        }
+    }
+
+    private void printOrderedCartItems(List<CartLine> items){
+        int i = 1;
+        for(CartLine item : items){
+            console.writeMessage(i + ". " + item.getItem().getName() + "  x " + item.getQuantity()); // 1. Water  x 2
+            i++;
+        }
     }
 
 }
